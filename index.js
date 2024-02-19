c1713d46e2fa595f063eb16aedf32226eab4338d
Liinares <miguellinares908@gmail.com>
2024-02-19 12:42:22 +0100

    añadiendo el jsonParser de express y añadiendo nuevos handlers de llamadas

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
-
 const app = express()
 
-const persons = [
+app.use(express.json())
+
+let persons = [
     { 
       "id": 1,
       "name": "Arto Hellas", 
@@ -33,8 +34,46 @@ app.get('/api/persons',(request, response) => {
     response.json(persons)
 })
 
+app.get('/api/persons/:id',(request, response) => {
+    const id = Number(request.params.id)
+    console.log("id: ", id)
+    const person = persons.find(person => person.id === id)
+    
+    if (person){
+        console.log("encontrado")
+        response.send(person)
+    } else{
+        console.log("no encontrado")
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id',(request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)    
+        
+    response.status(204).end()
+})
+
+app.post('/api/persons',(request, response) => {
+  const person = request.body;
+
+  const ids = persons.map(person => person.id)
+  const maxId = Math.max(...ids)
+
+  const newPerson = {
+    id: maxId + 1,
+    name: person.name,
+    number: person.number
+  }
+
+  persons = [...persons, newPerson ]
+
+  response.json(newPerson)
+})
+
 const PORT = 3001
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-})
+}) 
