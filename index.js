00059a8bab210537dc3338f0005a97fed884523a
Liinares <miguellinares908@gmail.com>
2024-02-22 11:29:44 +0100

    incluyendo el uso de mongoDB al GET del /api/persons

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+require('./mongo')
+
+const Person = require('./models/Person')
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
@@ -18,35 +21,17 @@ app.use(morgan(function (tokens, req, res) {
   ].join(' ')
 }))
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456'
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523'
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345'
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122'
-  }
-]
+let persons = []
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello world!</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({})
+    .then(notes => {
+      response.json(notes)
+    })
 })
 
 app.get('/info', (request, resposne) => {
