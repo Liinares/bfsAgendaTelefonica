bc872238c5d274995a3f86aaa19ce7620cd88eb5
Liinares <miguellinares908@gmail.com>
2024-02-28 10:07:48 +0100

    corrigiendo fallo en config

--- utils/config.js
+++ utils/config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const PORT = process.env.PORT
 const MONGO_DB_URI = process.env.MONGO_DB_URI
-const MONGO_DB_URI_TEST = process.MONGO_DB_URI_TEST
+const MONGO_DB_URI_TEST = process.env.MONGO_DB_URI_TEST
 const NODE_ENV = process.env.NODE_ENV
 
 module.exports = {
