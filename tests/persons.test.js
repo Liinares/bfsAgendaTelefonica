7a317d625aaa10ce2dd596cf71e855479cf203dc
Liinares <miguellinares908@gmail.com>
2024-02-29 08:44:53 +0100

    refactorizando beforeEach y añadiendo describe para agurpar tests

--- tests/persons.test.js
+++ tests/persons.test.js
@@ -7,99 +7,106 @@ beforeEach(async () => {
   await Person.deleteMany({}) // delete all Person in test database
 
   // Create an inital state of database
-  const person1 = new Person(initialPersons[0])
-  await person1.save()
-  const person2 = new Person(initialPersons[1])
-  await person2.save()
+  const personObjects = initialPersons.map(person => new Person(person))
+  const promises = personObjects.map(person => person.save())
+  await Promise.all(promises)
 }, 10000)
 
-test('notes are returned as json', async () => {
-  await api
-    .get('/api/persons')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-}, 10000)
+describe('GET person', () => {
+  test('notes are returned as json', async () => {
+    await api
+      .get('/api/persons')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  }, 10000)
 
-test('there are two notes', async () => {
-  const response = await api.get('/api/persons')
-  expect(response.body).toHaveLength(initialPersons.length)
-}, 10000)
+  test('there are two notes', async () => {
+    const response = await api.get('/api/persons')
+    expect(response.body).toHaveLength(initialPersons.length)
+  }, 10000)
 
-test('the first person is Fran', async () => {
-  const names = (await getAllNamesFromPersons()).names
+  test('the first person is Fran', async () => {
+    const names = (await getAllNamesFromPersons()).names
 
-  expect(names).toContain('Fran')
-}, 10000)
-
-test('a valid person can be added', async () => {
-  const newPerson = {
-    name: 'NuevaPersona',
-    number: '142-3453325'
-  }
-
-  await api
-    .post('/api/persons')
-    .send(newPerson)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const { names, response } = await getAllNamesFromPersons()
-
-  expect(response.body).toHaveLength(initialPersons.length + 1)
-  expect(names).toContain(newPerson.name)
-}, 10000)
-
-test('a person without name cant be added', async () => {
-  const newPerson = {
-    number: '142-3453325'
-  }
-
-  await api
-    .post('/api/persons')
-    .send(newPerson)
-    .expect(400)
-    .expect('Content-Type', /application\/json/)
-}, 10000)
-
-test('a person can be deleted', async () => {
-  const { response: firstResponse } = await getAllNamesFromPersons()
-  const { body: persons } = firstResponse
-  const personToDelete = persons[0]
-
-  await api
-    .delete(`/api/persons/${personToDelete.id}`)
-    .expect(204)
-
-  const { response: secondResponse } = await getAllNamesFromPersons()
-  expect(secondResponse.body).toHaveLength(initialPersons.length - 1)
+    expect(names).toContain('Fran')
+  }, 10000)
 })
 
-test('delete with incorrect id returns 400 ', async () => {
-  const id = '2142'
+describe('POST person', () => {
+  test('a valid person can be added', async () => {
+    const newPerson = {
+      name: 'NuevaPersona',
+      number: '142-3453325'
+    }
 
-  await api
-    .delete(`/api/persons/${id}`)
-    .expect(400)
+    await api
+      .post('/api/persons')
+      .send(newPerson)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const { names, response } = await getAllNamesFromPersons()
+
+    expect(response.body).toHaveLength(initialPersons.length + 1)
+    expect(names).toContain(newPerson.name)
+  }, 10000)
+
+  test('a person without name cant be added', async () => {
+    const newPerson = {
+      number: '142-3453325'
+    }
+
+    await api
+      .post('/api/persons')
+      .send(newPerson)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+  }, 10000)
 })
 
-test('a person name and phone can be changed', async () => {
-  const { response: firstResponse } = await getAllNamesFromPersons()
-  const { body: persons } = firstResponse
-  const personToEdit = persons[0]
+describe('DELETE person', () => {
+  test('a person can be deleted', async () => {
+    const { response: firstResponse } = await getAllNamesFromPersons()
+    const { body: persons } = firstResponse
+    const personToDelete = persons[0]
 
-  const editedPerson = {
-    name: 'Francho',
-    number: '12-111111'
-  }
+    await api
+      .delete(`/api/persons/${personToDelete.id}`)
+      .expect(204)
 
-  await api
-    .put(`/api/persons/${personToEdit.id}`)
-    .send(editedPerson)
-    .expect(200)
-    .then(response => {
-      expect(response.body.name).toBe(editedPerson.name)
-      expect(response.body.number).toBe(editedPerson.number)
-    })
+    const { response: secondResponse } = await getAllNamesFromPersons()
+    expect(secondResponse.body).toHaveLength(initialPersons.length - 1)
+  })
+
+  test('delete with incorrect id returns 400 ', async () => {
+    const id = '2142'
+
+    await api
+      .delete(`/api/persons/${id}`)
+      .expect(400)
+  })
+})
+
+describe('PUT person', () => {
+  test('a person name and phone can be changed', async () => {
+    const { response: firstResponse } = await getAllNamesFromPersons()
+    const { body: persons } = firstResponse
+    const personToEdit = persons[0]
+
+    const editedPerson = {
+      name: 'Francho',
+      number: '12-111111'
+    }
+
+    await api
+      .put(`/api/persons/${personToEdit.id}`)
+      .send(editedPerson)
+      .expect(200)
+      .then(response => {
+        expect(response.body.name).toBe(editedPerson.name)
+        expect(response.body.number).toBe(editedPerson.number)
+      })
+  })
 })
 
 afterAll(() => {
