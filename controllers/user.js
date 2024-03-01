c634c1f1b8a49adf47b9549d3475f1a5a72e72c6
Liinares <miguellinares908@gmail.com>
2024-03-01 18:09:58 +0100

    añadiendo metodo GET para users

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,11 @@ const userRouter = require('express').Router()
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
 
+userRouter.get('/', async (request, response, next) => {
+  const users = await User.find({})
+  response.json(users)
+})
+
 userRouter.post('/', async (request, response, next) => {
   const { body } = request
   const { username, name, password } = body
