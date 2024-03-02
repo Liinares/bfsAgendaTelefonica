1e3146aa35ffde2bb2f907e62e9febb1480f4a2e
Liinares <miguellinares908@gmail.com>
2024-03-02 20:52:02 +0100

    añadiendo en populate al GET de users para los blogs

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,13 @@ const User = require('../models/User')
 const bcrypt = require('bcrypt')
 
 userRouter.get('/', async (request, response, next) => {
-  const users = await User.find({})
+  const users = await User.find({}).populate('blogs', {
+    title: 1,
+    author: 1,
+    likes: 1,
+    url: 1
+  })
+
   response.json(users)
 })
 
