5272b61ea9fdb3dc7d2de34b74f9578da749d8b7
Liinares <miguellinares908@gmail.com>
2024-03-04 08:29:35 +0100

    utilizando el jsonwebtoken para el login

--- controllers/login.js
+++ controllers/login.js
@@ -1,6 +1,8 @@
 const loginRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const User = require('../models/User')
+const config = require('../utils/config')
 
 loginRouter.post('/', async (request, response, next) => {
   const { body } = request
@@ -23,9 +25,17 @@ loginRouter.post('/', async (request, response, next) => {
     })
   }
 
+  const userForToken = {
+    id: user._id,
+    username: user.username
+  }
+
+  const token = jwt.sign(userForToken, config.SECRET)
+
   response.status(200).send({
     name: user.name,
-    username: user.username
+    username: user.username,
+    token
   })
 })
 
--- utils/config.js
+++ utils/config.js
@@ -4,10 +4,12 @@ const PORT = process.env.PORT
 const MONGO_DB_URI = process.env.MONGO_DB_URI
 const MONGO_DB_URI_TEST = process.env.MONGO_DB_URI_TEST
 const NODE_ENV = process.env.NODE_ENV
+const SECRET = process.env.SECRET
 
 module.exports = {
   MONGO_DB_URI,
   PORT,
   MONGO_DB_URI_TEST,
-  NODE_ENV
+  NODE_ENV,
+  SECRET
 }
