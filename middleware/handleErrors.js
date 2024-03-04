6ed3f2661dd99ff70f54f9f53e9f575eb124e520
Liinares <miguellinares908@gmail.com>
2024-03-04 10:18:59 +0100

    refactorizando el handleErrors

--- middleware/handleErrors.js
+++ middleware/handleErrors.js
@@ -1,17 +1,28 @@
+const ERROR_HANDLERS = {
+  CastError: (res, error) => {
+    res.status(400).send({
+      error: 'id used is malformed'
+    })
+  },
+  ValidationError: (res, error) => {
+    res.status(400).json({
+      error: error.message
+    })
+  },
+  JsonWebTokenError: (res, error) => {
+    res.status(401).json({ error: 'token missing or invalid' })
+  },
+  defaultError: res => res.status(500).end()
+}
+
 const handleErrors = (error, request, response, next) => {
   console.log('HANDLE ERRORS')
   console.log(error.name)
-  console.log(error)
+  console.error(error)
 
-  if (error.name === 'CastError') {
-    response.status(400).send({
-      error: 'id used is malformed'
-    })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  } else {
-    response.status(500).end()
-  }
+  const handler = ERROR_HANDLERS[error.name] || ERROR_HANDLERS.defaultError
+
+  handler(response, error)
 }
 
 module.exports = handleErrors
